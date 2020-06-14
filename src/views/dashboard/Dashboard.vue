c40c4ee8429b2d6db437e3e32366682f14e660a8
brambekkers <bram.bekkers@outlook.com>
2020-06-14 21:10:13 +0200

    Update dashboard

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -8,6 +8,7 @@
 					<div class="row">
 						<AdminInfo />
 						<TotalMaterials />
+						<TotalReservations />
 					</div>
 				</div>
 			</div>
@@ -24,9 +25,17 @@
 	// Panels
 	import AdminInfo from "@/components/dashboard/dashboardItems/AdminInfo.vue";
 	import TotalMaterials from "@/components/dashboard/dashboardItems/TotalMaterials.vue";
+	import TotalReservations from "@/components/dashboard/dashboardItems/TotalReservations.vue";
 
 	export default {
 		name: "Home",
-		components: { Sidebar, Navbar, Footer, AdminInfo, TotalMaterials }
+		components: {
+			Sidebar,
+			Navbar,
+			Footer,
+			AdminInfo,
+			TotalMaterials,
+			TotalReservations
+		}
 	};
 </script>
