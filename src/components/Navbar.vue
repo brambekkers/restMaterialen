a36871c022c2ff0cb61b4349672808826caac57d
Bram Bekkers <bram.bekkers@outlook.com>
2021-06-03 10:36:47 +0200

    navbar fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -38,7 +38,7 @@
 						>Onze missie</router-link
 					>
 				</li> -->
-				<li class="nav-item d-lg-none" v-if="user">
+				<li class="nav-item " v-if="user">
 					<router-link class="nav-link" to="/profile">Profiel</router-link>
 				</li>
 				<li class="nav-item d-lg-none" v-if="user">
@@ -57,75 +57,75 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-export default {
-	computed: {
-		...mapGetters(["user", "isEditor", "isAdmin"]),
-	},
-	methods: {
-		logout() {
-			this.$store.dispatch("logout");
-			this.$router.push("/libary");
+	import { mapGetters } from "vuex";
+	export default {
+		computed: {
+			...mapGetters(["user", "isEditor", "isAdmin"])
 		},
-	},
-};
+		methods: {
+			logout() {
+				this.$store.dispatch("logout");
+				this.$router.push("/libary");
+			}
+		}
+	};
 </script>
 
 <style lang="scss" scoped>
-.pointer {
-	cursor: pointer;
-}
-.icon-bar {
-	width: 22px;
-	height: 2px;
-	background-color: #b6b6b6;
-	display: block;
-	transition: all 0.2s;
-	margin-top: 4px;
-}
-
-.navbar-toggler {
-	border: none;
-	background: transparent !important;
-}
-.navbar-toggler .top-bar {
-	transform: rotate(45deg);
-	transform-origin: 10% 10%;
-}
-
-.navbar-toggler .middle-bar {
-	opacity: 0;
-}
-
-.navbar-toggler .bottom-bar {
-	transform: rotate(-45deg);
-	transform-origin: 10% 90%;
-}
-
-.navbar-toggler.collapsed .top-bar {
-	transform: rotate(0);
-}
-
-.navbar-toggler.collapsed .middle-bar {
-	opacity: 1;
-}
-
-.navbar-toggler.collapsed .bottom-bar {
-	transform: rotate(0);
-}
-
-.navbar-brand {
-	overflow-y: none;
-	max-height: 130px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-
-	.hmcLogo {
-		bottom: 2rem;
-		right: 2rem;
-		height: 200px;
-		max-width: calc(25px + 15vmin);
+	.pointer {
+		cursor: pointer;
 	}
-}
+	.icon-bar {
+		width: 22px;
+		height: 2px;
+		background-color: #b6b6b6;
+		display: block;
+		transition: all 0.2s;
+		margin-top: 4px;
+	}
+
+	.navbar-toggler {
+		border: none;
+		background: transparent !important;
+	}
+	.navbar-toggler .top-bar {
+		transform: rotate(45deg);
+		transform-origin: 10% 10%;
+	}
+
+	.navbar-toggler .middle-bar {
+		opacity: 0;
+	}
+
+	.navbar-toggler .bottom-bar {
+		transform: rotate(-45deg);
+		transform-origin: 10% 90%;
+	}
+
+	.navbar-toggler.collapsed .top-bar {
+		transform: rotate(0);
+	}
+
+	.navbar-toggler.collapsed .middle-bar {
+		opacity: 1;
+	}
+
+	.navbar-toggler.collapsed .bottom-bar {
+		transform: rotate(0);
+	}
+
+	.navbar-brand {
+		overflow-y: none;
+		max-height: 130px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		.hmcLogo {
+			bottom: 2rem;
+			right: 2rem;
+			height: 200px;
+			max-width: calc(25px + 15vmin);
+		}
+	}
 </style>
